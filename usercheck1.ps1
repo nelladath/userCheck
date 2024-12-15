9cf3be76b9d5a375f93f8ebc8806b502d2842905
Sujin Nelladath <Suji700@gmail.com>
2024-12-15 09:32:03 +0530

    First Commit - Committing all the files to the repository

---
+++ usercheck1.ps1
@@ -0,0 +1,26 @@
+﻿$domains = @("corp", "csgiutil", "prod")
+
+$allcreds = foreach ($Domain in $domains)
+{
+    $cred = Get-Credential $Domain
+
+    $obj = New-Object PSObject
+    Add-Member -InputObject $obj -MemberType NoteProperty -Name Domain -Value $Domain
+    $obj | Add-Member -MemberType NoteProperty -Name Cred -Value $cred
+
+    $Obj
+}
+
+$list = Get-Content 'C:\Sujin Data\Powershell\scripts\test.txt'
+
+$check = {get-localuser | where-object { $_.name -like "*test1*"}}
+
+foreach ($computer in $list)
+{
+    $dname = $computer.Split(".")[1]
+    $mycred = ($allcreds | Where-Object {$_.Domain -eq $dname}).cred
+
+    Invoke-Command -ScriptBlock $check -Credential $mycred -ComputerName $computer
+}
+
+
---
+++ usercheck2.ps1
@@ -0,0 +1,47 @@
+﻿cls
+$domains = @("corp", "csgiutil", "prod")
+
+$allcreds = foreach ($Domain in $domains)
+{
+    $cred = Get-Credential $Domain
+
+    $obj = New-Object PSObject
+    Add-Member -InputObject $obj -MemberType NoteProperty -Name Domain -Value $Domain
+    $obj | Add-Member -MemberType NoteProperty -Name Cred -Value $cred
+
+    $Obj
+}
+
+$list = Get-Content 'C:\Sujin Data\Powershell\scripts\test.txt'
+
+$check = {
+    try {
+        $bldusr = {get-localuser | where-object { $_.name -like "*test1*"}}
+        if ($bldusr)
+        {
+            Return $true
+        }
+        else
+        {
+            Return $false
+        }
+    }
+    catch
+    {
+        Return $false
+    }
+}
+
+$out = Invoke-Command -ComputerName localhost -ScriptBlock $check
+
+if ($out -eq $true)
+{
+
+foreach ($computer in $list)
+{
+    $dname = $computer.Split(".")[1]
+    $mycred = ($allcreds | Where-Object {$_.Domain -eq $dname}).cred
+
+    Invoke-Command -ScriptBlock $check -Credential $mycred -ComputerName $computer
+}
+}
